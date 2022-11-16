8142e90d904a2379352374dad7718039816d610a
Tajul Islam Refath <tajul16-447@diu.edu.bd>
2022-11-16 08:17:22 +0600

    Delete some file

--- Assignment_1/index.js
+++
@@ -1,5 +0,0 @@
-const { studentList } = require("./students");
-studentList.map((student, i) => {
-  let details = `Student Id : ${student.id} and Name is ${student.name}`;
-  console.log(details);
-});
