9af4bfe83853900facd75a71537cdc36e1c81cc2
Tajul Islam Refath <tajul16-447@diu.edu.bd>
2023-06-03 19:54:07 +0600

    add server

---
+++ server/src/models/UserModel.js
@@ -0,0 +1,16 @@
+const { Schema, model } = require("mongoose");
+
+const userSchema = new Schema(
+  {
+    email: {
+      type: String,
+      unique: true,
+    },
+    name: { type: String },
+    password: { type: String },
+  },
+  { timestamps: true }
+);
+
+const User = model("User", userSchema);
+module.exports = User;
